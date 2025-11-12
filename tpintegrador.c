0875ef4f61b12ede99c16727fbf350db53db4c7b
Alexis <Alexis_cdg16@hotmail.com>
2025-11-12 16:51:57 -0300

    Add files via upload
    Actualiza el código borrando todo lo relacionado a la libreria windows.h y los distintos "system("cls")" e inclui la opción enemiga de defenderse.

--- tpintegrador.c
+++ tpintegrador.c
@@ -16,8 +16,8 @@ Requisitos:
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <string.h>
+#include <stdbool.h>>
 
 typedef struct 
 {
@@ -25,7 +25,7 @@ typedef struct
     int vida_max;
     int vida_actual;
     int ataque;
-    int defensa;
+    bool defensa;
     int energia_max;
     int energia_actual;
     int pocion;
@@ -70,7 +70,7 @@ int main (int argc, char* argv[])
     {
         principal->nombre = strdup("Caballero");
         principal->ataque = 20;
-        principal->defensa = 50;
+        principal->defensa = FALSE;
         principal->vida_max = 100;
         principal->vida_actual = 100;
         principal->energia_max = 100;
@@ -82,7 +82,7 @@ int main (int argc, char* argv[])
     {
         enemigo_1->nombre = strdup("Orco común");
         enemigo_1->ataque = 8;
-        enemigo_1->defensa = 35;
+        enemigo_1->defensa = FALSE;
         enemigo_1->vida_max = 70;
         enemigo_1->vida_actual = 70;
         enemigo_1->energia_max = 70;
@@ -95,32 +95,19 @@ int main (int argc, char* argv[])
     while ((principal->vida_actual > 0) && (enemigo_1->vida_actual > 0))
     {
         turno_jugador(principal, enemigo_1);
-        
-        system ("cls");
 
         turno_enemigo(enemigo_1, principal);
-
-        system("cls");
     }
 
     if (principal->vida_actual <= 0)
     {
         printf("Haz muerto\n");
-        Sleep(2000);
-        fflush(stdout);
-        system("cls");
     }
     else
     {
         printf("Lograste derrotar al enemigo\n");
-        Sleep(2000);
-        fflush(stdout);
-        system("cls");
 
         printf("Eres libre de continuar tu aventura\n");
-        Sleep(2000);
-        fflush(stdout);
-        system("cls");
     }
 
     free (principal->nombre);
@@ -134,56 +121,22 @@ int main (int argc, char* argv[])
 
 void introduccion(void)
 {
-    /*escribir_lentamente("BIENVENIDO JUGADOR.", 40);
-
-    escribir_lentamente("LAS OPCIONES SE MANEJARAN CON LOS NÚMEROS CORRESPONDIENTES A CADA OPCION", 40);
-    
-    system("cls");
-
-    escribir_lentamente("Eres un viajero que recorre el mundo.", 40);
-
-    escribir_lentamente("Al comenzar tu viaje compras 3 pociones(40 de curacion)", 40);
-
-    escribir_lentamente("En tu viaje alrededor del mundo, te haz encontrado con un monstruo errante.", 40);
-
-    escribir_lentamente("¡Es un orco!", 40);
-
-    escribir_lentamente("¡Este te ataca inmediatamente!", 50);*/
 
     printf("BIENVENIDO JUGADOR\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
 
     printf("LAS OPCIONES SE MANEJARAN CON LOS NÚMEROS CORRESPONDIENTES A CADA OPCION\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
 
     printf("Eres un viajero que recorre el mundo.\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
 
     printf("Al comenzar tu viaje compras 3 pociones(40 de curacion)\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
 
     printf("En tu viaje alrededor del mundo, te haz encontrado con un monstruo errante.\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
 
     printf("¡Es un orco!\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
 
     printf("¡Este te ataca inmediatamente!\n");
-    Sleep(2000);
-    fflush(stdout);
-    system("cls");
+
+    printf("\n");
 
     return;
 }
@@ -194,7 +147,7 @@ void turno_jugador (personaje_t* principal, personaje_t* enemigo)
 
     while ((opciones_pelea != 1) && (opciones_pelea != 2) && (opciones_pelea != 3))
     {
-        printf("1-Atacar ..... 2- Ataque fuerte(40 energia)\n 3- Usar poción\n");
+        printf("1-Atacar ..... 2- Ataque fuerte(40 energia)\n3- Usar poción\n");
 
         printf("---------------------------------------------\n");
 
@@ -208,32 +161,54 @@ void turno_jugador (personaje_t* principal, personaje_t* enemigo)
 
         scanf("%d", &opciones_pelea);
 
-        system("cls");
-
         switch (opciones_pelea)
         {
             case 1:
-                enemigo->vida_actual = enemigo->vida_actual - principal->ataque;
+                if (enemigo->defensa == FALSE)
+                {
+                    enemigo->vida_actual = enemigo->vida_actual - principal->ataque;
             
-                printf("¡%s a recibido %d daño!\n", enemigo->nombre, principal->ataque);
-                fflush(stdout);
+                    printf("¡%s a recibido %d daño!\n", enemigo->nombre, principal->ataque);
 
-                Sleep(2500);
-                break;
+                    break;
+                }
+                else
+                {
+                    enemigo->vida_actual = enemigo->vida_actual - (principal->ataque / 2);
+            
+                    printf("¡%s a recibido %d daño!\n", enemigo->nombre, (principal->ataque / 2));
+
+                    break;
+                }
 
             case 2:
                 if (principal->energia_actual >= 40)
                 {
-                    enemigo->vida_actual = enemigo->vida_actual - (principal->ataque * 2);
+                    if (enemigo->defensa == FALSE)
+                    {
+                        enemigo->vida_actual = enemigo->vida_actual - (principal->ataque * 2);
 
-                    printf("¡%s a recibido %d daño!\n", enemigo->nombre, (principal->ataque * 2));
-                    fflush(stdout);
+                        printf("¡%s a recibido %d daño!\n", enemigo->nombre, (principal->ataque * 2));
+                        principal->energia_actual = principal->energia_actual - 40;
 
-                    Sleep(2500);
+                        break;
+                    }
+                    else
+                    {
+                        enemigo->vida_actual = enemigo->vida_actual - principal->ataque;
+
+                        printf("¡%s a recibido %d daño!\n", enemigo->nombre, principal->ataque);
+                        principal->energia_actual = principal->energia_actual - 40;
+
+                        break;
+                    }
+                    
                 }
                 else 
                 {
                     printf("Cantidad de energía insuficiente\n");
+
+                    break;
                 }
         
             case 3:
@@ -242,39 +217,42 @@ void turno_jugador (personaje_t* principal, personaje_t* enemigo)
                     if (principal->vida_actual > 70)
                     {
                         principal->vida_actual = 100;
+
                         printf("Te haz curado toda la vida\n");
+
+                        break;
                     }
                     else
                     {
                         principal->vida_actual = principal->vida_actual + 30;
-                        printf("Te haz curado 30 de vida\n");
-                        Sleep(2500);
 
-                        system("cls");
+                        printf("Te haz curado 30 de vida\n");
+                    
+                        break;
                     }
                     principal->pocion = principal->pocion - 1;
                 }
                 else
                 {
                     printf("No tienes mas pociones.\n");
-                    Sleep(1000);
-                    printf("Turno perdido\n");
-                    Sleep(2500);
 
-                    system("cls");
+                    printf("Turno perdido\n");
+
+                    break;
                 }
 
-                default:
-                    printf("Opción ingresada inválida. \n");
-                    Sleep(2500);
-
-                    system("cls");
+            default:
+                printf("Opción ingresada inválida. \n");
+                    
+                break;
         }
     }
 }
 
 void turno_enemigo (personaje_t* enemigo, personaje_t* principal)
 {
+    enemigo->defensa = FALSE;
+    
     if (enemigo->vida_actual <= 0)
     {
         return;
@@ -282,24 +260,19 @@ void turno_enemigo (personaje_t* enemigo, personaje_t* principal)
     else
     {
         if (enemigo->vida_actual <= (enemigo->vida_max / 5))
+        // 1/5 sera la referencia para "Vida baja"
         {
             if (enemigo->pocion > 0)
             {
                 if (enemigo->vida_actual >= (enemigo->vida_max - 20))
                 {
                     printf("%s se curo toda la vida", enemigo->nombre);
-                    fflush(stdout);
-
-                    Sleep(2500);
 
                     enemigo->vida_actual = enemigo->vida_max;
                 }
                 else
                 {
                     printf("¡%s se acaba de curar 20 de vida!\n", enemigo->nombre);
-                    fflush(stdout);
-
-                    Sleep(2500);
                 
                     enemigo->vida_actual = enemigo->vida_actual + 20;
                 }
@@ -308,46 +281,32 @@ void turno_enemigo (personaje_t* enemigo, personaje_t* principal)
             else
             {
                 printf("¡%s no pudo curarse y ataca agotado\n", enemigo->nombre);
-                fflush(stdout);
-
-                Sleep(2500);
 
                 principal->vida_actual = principal->vida_actual - (enemigo->ataque / 2);
 
                 printf("Recibes %d daño",((enemigo->ataque / 2)));
-                fflush(stdout);
-
-                Sleep(2500);
             }
+        }
+        else if (enemigo->energia_actual > 20)
+        //20 o menos será la referencia para "Energia baja"
+        {
+            printf("%s te ataca\n", enemigo->nombre);
+
+            principal->vida_actual = principal->vida_actual - enemigo->ataque;
+
+            enemigo->energia_actual = enemigo->energia_actual - 20;
+
+            printf("Recibes %d daño\n", enemigo->ataque);
         }
         else
         {
-            printf("%s te ataca\n", enemigo->nombre);
-            fflush(stdout);
+            printf("%s esta sin energia y se defiende\n", enemigo->nombre);
 
-            Sleep(2500);
-            principal->vida_actual = principal->vida_actual - enemigo->ataque;
+            enemigo->defensa = TRUE;
 
-            printf("Recibes %d daño\n", enemigo->ataque);
-            fflush(stdout);
+            enemigo->energia_actual = enemigo-> energia_max
 
-            Sleep(2500);
+            printf("%s Recupero toda la energia\n", enemigo->nombre);
         }
-
-        system ("cls");
     }
 }
-
-/*void escribir_lentamente(const char *cadena, int pausa) 
-{
-    for (size_t i = 0; i < strlen(cadena); i++) {
-        printf("%c", cadena[i]);
-        fflush(stdout); 
-        Sleep(pausa); 
-    }
-    printf("\n");
-
-    Sleep(2000);
-
-    system("cls");
-}*/
